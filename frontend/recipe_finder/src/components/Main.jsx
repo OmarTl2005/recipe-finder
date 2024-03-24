741260ec6fa1a9bfd3f1fa3281aaf022f9fa61f2
omarWRLD <63510451+OmarTl2005@users.noreply.github.com>
2024-03-24 12:23:57 +0000

    Refactor recipe filtering logic in Main component

--- frontend/recipe_finder/src/components/Main.jsx
+++ frontend/recipe_finder/src/components/Main.jsx
@@ -12,7 +12,12 @@ const Main = () => {
     const getRecipes = async () => {
       try {
         const response = await axios.get('http://localhost:5000/', { withCredentials: true });
-        setRecipes(response.data);
+        if (search) {
+          const filtered = response.data.filter((recipe) => recipe.title.toLowerCase().includes(search.toLowerCase()));
+          setRecipes(filtered);
+        } else {
+          setRecipes(response.data);
+        }
         console.log(response.data)
       } catch (error) {
         console.error(error.response?.data?.error || 'Error fetching recipes');
@@ -20,11 +25,11 @@ const Main = () => {
     };
 
     getRecipes();
-  }, []);
+  }, [search]);
   return (
-    <div className='flex flex-col items-center justify-between h-full w-[80%] self-center pb-20 m-0 px-0'>
+    <div className='flex flex-col items-center justify-between h-screen w-[80%] self-center pb-20 m-0 px-0'>
       {recipes.length > 0 ? (
-          <ul className='flex text-center justify-center flex-wrap w-full h-full gap-[100px]'> 
+          <ul className='flex text-center justify-center flex-wrap w-full h-full gap-[100px] max-h-[100px]'> 
           {recipes.map((recipe) => (
             <li className='flex flex-col justify-center gap-1 isolate aspect-video shadow-white shadow-3xl p-8 rounded-[17%] bg-gradient-to-b from-blue-300/30' key={recipe.id}>
               <img className='rounded-full' width='200px' height='200px' src={`http://localhost:5000/uploads/${recipe.filename}`} alt={recipe.title} />
