82c582ed489f34c8a6c75c152a5da031ab824d07
omarWRLD <63510451+OmarTl2005@users.noreply.github.com>
2024-03-31 02:10:57 +0000

    adding documentation to the code

--- backend/app.py
+++ backend/app.py
@@ -30,8 +30,8 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 # managin models
+# User Model
 class User(UserMixin, db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(100), unique=True)
@@ -42,14 +42,15 @@ class User(UserMixin, db.Model):
         return f'<User {self.username}>'
 
     def make_password(self, password):
+        """Hashes the user's password."""
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
+        """Checks if the provided password matches the user's hashed password."""
         return check_password_hash(self.password, password)
 
-# Define Recipe model
+# Recipe Model
 class Recipe(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
@@ -59,12 +60,12 @@ class Recipe(db.Model):
     filename = db.Column(db.String(255), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     ingredients = db.relationship('Ingredient', backref='recipe', lazy='dynamic')
-    comments = db.relationship('Comments', backref='recipe', lazy='dynamic')
+    comments = db.relationship('Comment', backref='recipe', lazy='dynamic')
 
     def __repr__(self):
         return f'<Recipe {self.title}>'
 
-# Define Ingredient model
+# Ingredient Model
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ingredient = db.Column(db.Text)
@@ -74,7 +75,8 @@ class Ingredient(db.Model):
     def __repr__(self):
         return f'<Ingredient {self.id}>'
 
-class Comments(db.Model):
+# Comment Model
+class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.Text)
     username = db.Column(db.Text)
@@ -83,10 +85,11 @@ class Comments(db.Model):
     def __repr__(self):
         return f'<Comment {self.id}>'
 
-# managing routes
+# Routes
 
 @app.route('/register', methods=['POST'])
 def register():
+    """Registers a new user."""
     data = request.json
     username = data.get('username')
     email = data.get('email')
@@ -100,18 +103,19 @@ def register():
     db.session.add(user)
     db.session.commit()
     
-    return jsonify({'message': 'User registrated successfully'}), 200
+    return jsonify({'message': 'User registered successfully'}), 200
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    """Logs in a user."""
     if request.method == 'GET':
         if current_user.is_authenticated:
-            return jsonify({'message': 'User already loged in!'}), 200
+            return jsonify({'message': 'User already logged in!'}), 200
         else:
-            return jsonify({'error': 'User not loged in!'}), 401
+            return jsonify({'error': 'User not logged in!'}), 401
     if request.method == 'POST':
         if current_user.is_authenticated:
-            return jsonify({'message': 'User already loged in!'}), 200
+            return jsonify({'message': 'User already logged in!'}), 200
     
         data = request.json
         email = data.get('email')
@@ -128,14 +132,16 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """Logs out the current user."""
     if current_user.is_authenticated:
         logout_user()
-        return jsonify({'message': 'User loged out!'}), 200
+        return jsonify({'message': 'User logged out!'}), 200
     else:
-        return jsonify({'error': 'User already loged out!'}), 401
+        return jsonify({'error': 'User already logged out!'}), 401
 
 @app.route('/')
 def index():
+    """Fetches all recipes."""
     try:
         recipes = Recipe.query.all()
         return jsonify([{'id': recipe.id,
@@ -152,6 +158,7 @@ def index():
 @app.route('/recipes')
 @login_required
 def recipes():
+    """Fetches recipes created by the current user."""
     try:
         recipes = Recipe.query.filter_by(user_id=current_user.id).all()
         return jsonify([{'id': recipe.id,
@@ -166,6 +173,7 @@ def recipes():
 
 @app.route('/uploads/<filename>')
 def serve_image(filename):
+    """Serves uploaded images."""
     try:
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         return send_file(filepath)
@@ -174,26 +182,22 @@ def serve_image(filename):
     
 @app.route('/media/<filename>')
 def get_media(filename):
+    """Fetches media files."""
     try:
         filepath = os.path.join('media', filename)
         return send_file(filepath)
     except:
-        return jsonify({'error': 'Image not found'}), 404
-
-import os
-import random
-from flask import request, jsonify
-from werkzeug.utils import secure_filename
-
-# ... (rest of your Flask app imports)
+        return jsonify({'error': 'Media file not found'}), 404
 
 @app.route('/make-recipe', methods=['POST'])
 @login_required
 def make_recipe():
+    """Creates a new recipe."""
     if request.method == 'POST':
         uploaded_file = request.files.get('file')
         try:
             if uploaded_file:
+                # Handle uploaded file
                 filename = secure_filename(uploaded_file.filename)
                 # Check for existing filenames and modify if needed
                 base, extension = os.path.splitext(filename)
@@ -211,68 +215,66 @@ def make_recipe():
                 filepath = os.path.join(uploads_dir, filename)
                 uploaded_file.save(filepath)
 
+                # Get recipe details
                 title = request.form.get('title')
                 description = request.form.get('description')
                 cuisine = request.form.get('cuisine')
 
+                # Create new recipe instance
                 new_recipe = Recipe(title=title, description=description, cuisine=cuisine, filename=filename, user_id=current_user.id)
 
                 db.session.add(new_recipe)
                 db.session.commit()
 
-                # Retrieve ingredients and instructions as lists
+                # Retrieve and add ingredients to the recipe
                 ingredients = request.form.getlist('ingredient')
                 instructions = request.form.getlist('instruction')
 
-                # Create and add ingredients to the recipe
                 for ingredient, instruction in zip(ingredients, instructions):
                     new_ingredient = Ingredient(ingredient=ingredient, instructions=instruction, recipe_id=new_recipe.id)
                     db.session.add(new_ingredient)
 
-                # Commit the ingredients
                 db.session.commit()
 
                 return jsonify({'message': 'Recipe created successfully!'}), 200
             else:
-                return jsonify({'error': 'No file uploaded!'}), 400  # Bad request
+                return jsonify({'error': 'No file uploaded!'}), 400
         except:
             return jsonify({'error': 'An error occurred while creating the recipe.'}), 500
 
-
-
-
+# Delete Recipe Route
 @app.route('/recipes/<int:recipe_id>', methods=['DELETE'])
 @login_required
 def delete_recipe(recipe_id):
-  """Deletes a recipe based on its ID."""
-  try:
-    recipe = Recipe.query.get(recipe_id)
-    ingredients = Ingredient.query.filter_by(recipe_id=recipe_id).all()
-    if not recipe:
-      return jsonify({'error': 'Recipe not found'}), 404
+    """Deletes a recipe based on its ID."""
+    try:
+        recipe = Recipe.query.get(recipe_id)
+        ingredients = Ingredient.query.filter_by(recipe_id=recipe_id).all()
+        if not recipe:
+            return jsonify({'error': 'Recipe not found'}), 404
 
-    if recipe.user_id != current_user.id:
-      return jsonify({'error': 'Unauthorized to delete this recipe'}), 403
+        if recipe.user_id != current_user.id:
+            return jsonify({'error': 'Unauthorized to delete this recipe'}), 403
 
-    # Delete the uploaded file from the file system if it exists
-    if recipe.filename:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], recipe.filename)
-        if os.path.exists(file_path):
-            os.remove(file_path)
-    
-    db.session.delete(recipe)
-    db.session.delete(ingredients)
-    # ... (Optional: Delete the uploaded file if needed)
+        # Delete the uploaded file from the file system if it exists
+        if recipe.filename:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], recipe.filename)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        
+        db.session.delete(recipe)
+        db.session.delete(ingredients)
+        db.session.commit()
 
-    db.session.commit()
-    return jsonify({'message': 'Recipe deleted successfully'}), 200  
-  except:
-    return jsonify({'error': 'An error occurred while deleting the recipe'}), 500 
-  return jsonify({'error': 'Method not allowed'}), 405 
+        return jsonify({'message': 'Recipe deleted successfully'}), 200  
+    except:
+        return jsonify({'error': 'An error occurred while deleting the recipe'}), 500 
 
+# Favorite Recipe Route
 @app.route('/favorite/<int:recipe_id>', methods=['PUT'])
 @login_required
 def favorite(recipe_id):
+    """Adds or removes a recipe from favorites."""
     try:
         recipe = Recipe.query.get(recipe_id)
         if not recipe:
@@ -282,7 +284,6 @@ def favorite(recipe_id):
         if is_favorite is None:
             return jsonify({'error': 'is_favorite field is required'}), 400
 
-        # Handle both true and false cases
         if is_favorite == 'true':
             recipe.favorite = True
             message = 'Recipe added to favorites successfully'
@@ -294,10 +295,12 @@ def favorite(recipe_id):
 
         return jsonify({'message': message}), 200
     except:
-        return jsonify({'error': 'something went wrong'}), 500
+        return jsonify({'error': 'Something went wrong'}), 500
 
+# Fetch Recipe Route
 @app.route('/recipe/<int:id>')
 def recipe(id):
+    """Fetches a single recipe by ID."""
     try:
         recipes = Recipe.query.filter_by(id=id).all()
         return jsonify([{'id': recipe.id,
@@ -309,33 +312,34 @@ def recipe(id):
     except:
         return jsonify({'error': 'An error occurred while fetching recipes'}), 500
 
+# Rate Recipe Route
 @app.route('/rating/<int:recipe_id>', methods=['POST'])
 @login_required
 def rate_recipe(recipe_id):
-  try:
-    # Get request data
-    data = request.json
-    rating = data.get('rating')
+    """Submits a rating for a recipe."""
+    try:
+        data = request.json
+        rating = data.get('rating')
 
-    if not rating:
-      return jsonify({'message': 'Missing rating data'}), 400
+        if not rating:
+            return jsonify({'message': 'Missing rating data'}), 400
 
-    # Find the recipe by ID
-    recipe = Recipe.query.filter_by(id=recipe_id).first()
-    if not recipe:
-      return jsonify({'message': 'Recipe not found'}), 404
+        recipe = Recipe.query.filter_by(id=recipe_id).first()
+        if not recipe:
+            return jsonify({'message': 'Recipe not found'}), 404
 
-    # Create a new Rating record
-    recipe.rating = rating
-    db.session.commit()
+        recipe.rating = rating
+        db.session.commit()
 
-    return jsonify({'message': 'Rating submitted successfully'}), 200
-  except:
-    return jsonify({'message': 'Error submitting rating'}), 500
+        return jsonify({'message': 'Rating submitted successfully'}), 200
+    except:
+        return jsonify({'message': 'Error submitting rating'}), 500
 
 
+# Ingredients Route
 @app.route('/ingredients/<int:recipe_id>')
 def ingredients(recipe_id):
+    """Fetches ingredients for a specific recipe."""
     try:
         my_ingredients = Ingredient.query.filter_by(recipe_id=recipe_id).all()
         ingredient_list = [{'id': ingredient.id, 'ingredient': ingredient.ingredient, 'instruction': ingredient.instructions} for ingredient in my_ingredients]
@@ -343,9 +347,11 @@ def ingredients(recipe_id):
     except:
         return jsonify({'error': 'An error occurred while fetching ingredients'}), 500
 
+# Add Comment Route
 @app.route('/add-comment/<int:recipe_id>', methods=['POST'])
 @login_required
 def add_comment(recipe_id):
+    """Adds a comment to a recipe."""
     try:
         data = request.json
         comment = data.get('comment')
@@ -357,8 +363,10 @@ def add_comment(recipe_id):
     except:
         return jsonify({'error': 'An error occurred while adding comment'}), 500
 
+# Comments Route
 @app.route('/comments/<int:recipe_id>')
 def comments(recipe_id):
+    """Fetches comments for a specific recipe."""
     try:
         my_comments = Comments.query.filter_by(recipe_id=recipe_id).all()
         comment_list = [{'id': comment.id, 'comment': comment.comment, 'username': comment.username} for comment in my_comments]
@@ -366,9 +374,11 @@ def comments(recipe_id):
     except:
         return jsonify({'error': 'An error occurred while fetching comments'}), 500
 
+# Delete Comment Route
 @app.route('/delete-comment/<int:comment_id>', methods=['DELETE'])
 @login_required
 def delete_comment(comment_id):
+    """Deletes a comment by its ID."""
     try:
         comment = Comments.query.get(comment_id)
         if not comment:
@@ -379,9 +389,11 @@ def delete_comment(comment_id):
     except:
         return jsonify({'error': 'An error occurred while deleting comment'}), 500
 
+# Get User Route
 @app.route('/get-user')
 @login_required
 def get_user():
+    """Fetches the current user's information."""
     return jsonify({'username': current_user.username}), 200
 
 if __name__ == '__main__':
